5db508834e79922a7e1961acffb14aa3c07ede80
YutoMizutani <yuto.mizutani.dev@gmail.com>
2019-04-19 01:47:09 +0900

    :bug: Fix router settings

--- Server/servo/Sources/App/app.swift
+++ Server/servo/Sources/App/app.swift
@@ -2,9 +2,10 @@ import Vapor
 
 /// Creates an instance of `Application`. This is called from `main.swift` in the run target.
 public func app(_ env: Environment) throws -> Application {
-    let config = Config.default()
-    let env = env
-    let services = Services.default()
+    var config = Config.default()
+    var env = env
+    var services = Services.default()
+    try configure(&config, &env, &services)
     let app = try Application(config: config, environment: env, services: services)
     try boot(app)
     return app
